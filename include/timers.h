0547caf7562563f1efe4974f3fa56630eeda7dbc
Tamim101 <tamimkhan7133@gmail.com>
2025-11-03 18:32:38 +0600

    update timer compare

--- include/timers.h
+++ include/timers.h
@@ -2,5 +2,6 @@
 #define TIM2_H_
 #define sr_uif   (1U<<0)
 void tim2_1hz_init(void);
+void tim2_pa5_output_compare(void);
 
 #endif
--- src/12_timers_main.c
+++ src/12_timers_main.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-#include <stdint.h>
-#include "stm32f4xx.h"
-#include "uart.h"
-#include "adc.h"
-#include  "systick.h"
-#include "timers.h"
+// #include <stdio.h>
+// #include <stdint.h>
+// #include "stm32f4xx.h"
+// #include "uart.h"
+// #include "adc.h"
+// #include  "systick.h"
+// #include "timers.h"
 
-#define gpioaen         (1U<<0)
-#define pin5            (1U<<5)
-#define led_pin         pin5
+// #define gpioaen         (1U<<0)
+// #define pin5            (1U<<5)
+// #define led_pin         pin5
 
 
 
-int main(void){
-    RCC->AHB1ENR |= gpioaen;
-    GPIOA->MODER |= (1U<<10);
-    GPIOA->MODER &=~ (1U<<11);
-    for(;;){
-        while(!(TIM2->SR & sr_uif)){
+// int main(void){
+//     RCC->AHB1ENR |= gpioaen;
+//     GPIOA->MODER |= (1U<<10);
+//     GPIOA->MODER &=~ (1U<<11);
+//     for(;;){
+//         while(!(TIM2->SR & sr_uif)){
 
-        }
-            printf("A secound passed !! \n \r");
-            GPIOA->ODR ^= led_pin;
+//         }
+//             printf("A secound passed !! \n \r");
+//             GPIOA->ODR ^= led_pin;
           
         
-    }
-}
+//     }
+// }
