82dcd90b6581167bd05f57bfa34a80bd8dcb74e6
Tamim101 <tamimkhan7133@gmail.com>
2025-11-03 17:26:15 +0600

    fix and update timers hardware

--- src/12_timers.c
+++ src/12_timers.c
@@ -4,10 +4,9 @@
 #define cr1_cen      (1U<<0)
 #include "timers.h"
 void tim2_1hz_init(void){
-    RCC->AHB1ENR |= timer2en;  // clock access to tim2
+    RCC->APB1ENR |= timer2en;  // clock access to tim2
     TIM2->PSC  = 1600 - 1;   //1600000 / 1600 = 10000
     TIM2->ARR  = 1000 - 1;   //100000 / 100000 = 1
-    TIM2-> CNT  = 0 ;
-    TIM2-> CR1  = cr1_cen;
-
+    TIM2->CNT  = 0;
+    TIM2->CR1  = cr1_cen;
 }
