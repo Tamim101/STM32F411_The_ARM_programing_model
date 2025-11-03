342d3af574559374539488033bb29315da5d71ae
Tamim101 <tamimkhan7133@gmail.com>
2025-11-03 20:30:25 +0600

    input capter added

--- include/timers.h
+++ include/timers.h
@@ -1,7 +1,9 @@
 #ifndef TIM2_H_
 #define TIM2_H_
 #define sr_uif   (1U<<0)
+#define sr_cc1f   (1U<<1)
 void tim2_1hz_init(void);
-void tim2_pa5_output_compare(void);
+void tim3_pa5_input_copter(void);
+void tim3_pa6_output_compare(void);
 
 #endif
