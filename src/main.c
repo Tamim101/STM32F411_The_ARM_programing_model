46d15bcd961a9aca63c9332b7610ddace245f0a9
Tamim101 <tamimkhan7133@gmail.com>
2026-01-17 20:48:57 +0600

    fix

--- src/main.c
+++ src/main.c
@@ -73,17 +73,17 @@
 //     volatile uint32_t APB1ENR;
 // }RCC_TypeDef;
 // typedef struct {
-//     volatile uint32_t CRL;
-//     volatile uint32_t CRH;
-//     volatile uint32_t IDR;
-//     volatile uint32_t ODR;
-//     volatile uint32_t BSRR;
-//     volatile uint32_t BRR;
-//     volatile uint32_t LCKR;
-// }GPIO_TypeDef;
+    volatile uint32_t CRL;
+    volatile uint32_t CRH;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t BRR;
+    volatile uint32_t LCKR;
+}GPIO_TypeDef;
 
-// #define RCC      ((RCC_TypeDef*)RCC_BASE)
-// #define GPIOA    ((GPIO_TypeDef*)GPIOA_BASE)
+#define RCC      ((RCC_TypeDef*)RCC_BASE)
+#define GPIOA    ((GPIO_TypeDef*)GPIOA_BASE)
 
 #include "stm32f1xx.h"
 
