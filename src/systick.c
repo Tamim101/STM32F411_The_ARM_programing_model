b79afd5d53393004299531620e6a606446a4db88
Tamim101 <tamimkhan7133@gmail.com>
2026-02-02 20:26:32 +0600

    update system time

---
+++ src/systick.c
@@ -0,0 +1,10 @@
+#include "stm32f1xx.h"
+
+#define SYSTICK_LOAD_VAL      16000
+#define CTRL_ENABLE           (1U<<0)
+#define CTRL_CLKSRC           (1U<<2)
+#define CTRL_COUNTFLAG        (1U<<16)
+
+void systickDelayMs(int n){
+    SysTick->LOAD = SYSTICK_LOAD_VAL;
+}
