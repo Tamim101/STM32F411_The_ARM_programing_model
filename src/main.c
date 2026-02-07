be2b5ab22f684c039066a8c9e337e46d928a2d56
Tamim101 <tamimkhan7133@gmail.com>
2026-02-07 19:47:48 +0600

    update

--- src/main.c
+++ src/main.c
@@ -273,21 +273,21 @@
 
 // }
 
-// #include <stdio.h>
-// #include <stdint.h>
-// #include "stm32f1xx.h"
-// #include "uart.h"
-// #include "adc.h"
-// uint32_t sensor_value;
-// int main(void){
-//     uart2_tx_init();
-//     pal_adc_int();
-//     start_converstion();
-//     while(1){
-//         sensor_value = adc_read();
-//         printf("Sensor value : %d \n\r",sensor_value);
-//     }
-// }
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f1xx.h"
+#include "uart.h"
+#include "adc.h"
+uint32_t sensor_value;
+int main(void){
+    uart2_tx_init();
+    pal_adc_int();
+    start_converstion();
+    while(1){
+        sensor_value = adc_read();
+        printf("Sensor value : %d \n\r",sensor_value);
+    }
+}
 
 
 
