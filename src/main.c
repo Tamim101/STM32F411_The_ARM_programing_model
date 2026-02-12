b2a900a3d306e97c116382c5196d0fdde6ac8c4a
Tamim101 <tamimkhan7133@gmail.com>
2026-02-12 21:08:04 +0600

    update main

--- src/main.c
+++ src/main.c
@@ -316,11 +316,6 @@ int main(void)
         printf("\a");
         delay(500000);
     }
-     while (1) {
-        GPIOA->ODR ^= LED_PIN;
-        printf("\a");
-        delay(500000);
-    }
-   
+  
 }
 
