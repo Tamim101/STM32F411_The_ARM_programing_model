66221573c352f65bcc766dabc0a67b5317163cbe
Tamim101 <tamimkhan7133@gmail.com>
2026-04-10 19:10:32 +0600

    add loop

--- src/main.c
+++ src/main.c
@@ -1,23 +1,23 @@
-#define PERIPH_BASE                  (0x40000000UL)
-#define AHBPERIPH_OFFSET             (0X00020000UL)
-#define AHBPERIPH_BASE               (PERIPH_BASE + AHBPERIPH_OFFSET)
-#define GPIOA_OFFSET                 (0x0000UL)
+// #define PERIPH_BASE                  (0x40000000UL)
+// #define AHBPERIPH_OFFSET             (0X00020000UL)
+// #define AHBPERIPH_BASE               (PERIPH_BASE + AHBPERIPH_OFFSET)
+// #define GPIOA_OFFSET                 (0x0000UL)
 
-#define GPIOA_BASE                   (AHBPERIPH_BASE + GPIOA_OFFSET)
+// #define GPIOA_BASE                   (AHBPERIPH_BASE + GPIOA_OFFSET)
 
-#define RCC_OFFSET                   (0x3800UL)
-#define RCC_BASE                     (AHBPERIPH_BASE + RCC_OFFSET)
+// #define RCC_OFFSET                   (0x3800UL)
+// #define RCC_BASE                     (AHBPERIPH_BASE + RCC_OFFSET)
 
-#define  AHB1EN_R_OFFSET             (0x30UL)
-#define  RCC_AHB1EN_R                (*(volatile unsigned int *) (RCC_BASE + AHB1EN_R_OFFSET))
-#define  GPIOA_MODE_R_OFFSET         (0x00UL)
-#define  GPIOA_MODE_R                (*(volatile unsigned int *) (GPIOA_BASE + GPIOA_MODE_R_OFFSET))
+// #define  AHB1EN_R_OFFSET             (0x30UL)
+// #define  RCC_AHB1EN_R                (*(volatile unsigned int *) (RCC_BASE + AHB1EN_R_OFFSET))
+// #define  GPIOA_MODE_R_OFFSET         (0x00UL)
+// #define  GPIOA_MODE_R                (*(volatile unsigned int *) (GPIOA_BASE + GPIOA_MODE_R_OFFSET))
 
-#define OD_R_OFFSET                  (0x14UL)
-#define GPIOA_OD_R                   (*(volatile unsigned int *) (GPIOA_BASE + OD_R_OFFSET))
+// #define OD_R_OFFSET                  (0x14UL)
+// #define GPIOA_OD_R                   (*(volatile unsigned int *) (GPIOA_BASE + OD_R_OFFSET))
 
-#define GPIOAEN                      (1U<<0)
-#define LED_PIN                      PIN5
+// #define GPIOAEN                      (1U<<0)
+// #define LED_PIN                      PIN5
 
 int main(void){
     RCC_AHB1EN_R |=  GPIOAEN;
