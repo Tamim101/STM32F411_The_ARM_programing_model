45b2374b95fcbfd1428de498f58cba7b741e054b
Tamim101 <tamimkhan7133@gmail.com>
2026-05-23 23:00:57 +0600

    add interrupt

--- src/interrupt.c
+++ src/interrupt.c
@@ -43,23 +43,23 @@
 //     }
 // }
 
-#include <stdint.h>
-#define RCC_APB2ENR     (*(volatile uint32_t *)0x40021018)
-#define GPIOC_CRH       (*(volatile uint32_t *)0x40011004)
-#define GPIOC_ODR       (*(volatile uint32_t *)0x4001100C)
-int main(void){
-    RCC_APB2ENR |= (1<<4);
-    GPIOC_CRH &=~0xFF;
-    GPIOC_ODR |= 0x22;
-    while (1)
-    {
-        /* code */
-        GPIOC_ODR ^= (1<<8) | (1<<9);
-        for (int i = 0; i < 8000; i++)
-        {
-            /* code */
-        }
+// #include <stdint.h>
+// #define RCC_APB2ENR     (*(volatile uint32_t *)0x40021018)
+// #define GPIOC_CRH       (*(volatile uint32_t *)0x40011004)
+// #define GPIOC_ODR       (*(volatile uint32_t *)0x4001100C)
+// int main(void){
+//     RCC_APB2ENR |= (1<<4);
+//     GPIOC_CRH &=~0xFF;
+//     GPIOC_ODR |= 0x22;
+//     while (1)
+//     {
+//         /* code */
+//         GPIOC_ODR ^= (1<<8) | (1<<9);
+//         for (int i = 0; i < 8000; i++)
+//         {
+//             /* code */
+//         }
         
-    }
+//     }
     
-}
+// }
